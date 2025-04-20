dc13849ad1b952fa386a39ff79cfb85214ab8ef1
TCAGM-Dev <thomas@andringa.org>
2025-04-20 17:01:42 +0200

    Differentiate logs from ManualTest.java

--- src/test/java/io/github/tcagmdev/questionair/test/ManualTest.java
+++ src/test/java/io/github/tcagmdev/questionair/test/ManualTest.java
@@ -8,7 +8,7 @@ public class ManualTest {
 	public static void main(String[] args) throws Exception {
         DataManager dataManager = new DataManager(new TestDataSeeder());
 
-        App app = new App(dataManager, "questionair");
+        App app = new App(dataManager, "questionair-test");
 
         new CLIFrontend(app, System.in, System.out).start();
 	}
