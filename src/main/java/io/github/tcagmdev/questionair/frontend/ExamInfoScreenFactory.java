f5471e23937922346488d8347ccc3d757af2aa38
TCAGM-Dev <thomas@andringa.org>
2025-04-20 16:51:51 +0200

    Implement ExamInfoScreenFactory.java

--- src/main/java/io/github/tcagmdev/questionair/frontend/ExamInfoScreenFactory.java
+++ src/main/java/io/github/tcagmdev/questionair/frontend/ExamInfoScreenFactory.java
@@ -1,10 +1,13 @@
 package io.github.tcagmdev.questionair.frontend;
 
 import io.github.tcagmdev.questionair.data.Exam;
+import io.github.tcagmdev.questionair.data.LearningGoal;
+import io.github.tcagmdev.questionair.data.Question;
 import io.github.tcagmdev.statesmith.StateMachine;
 import io.github.tcagmdev.statesmith.StateNode;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public abstract class ExamInfoScreenFactory {
@@ -16,7 +19,37 @@ public abstract class ExamInfoScreenFactory {
 	}
 
 	public static StateNode<String> createNode(StateMachine<String> stateMachine, StateNode<String> parent, Exam exam) {
-		StateNode<String> node = stateMachine.addNode();
+		StateNode<String> node = stateMachine.addNode(_ -> {
+			System.out.printf("Module: %s, Version: %s%n", exam.getModule().getName(), exam.getVersion());
+			List<Exam.QuestionReference> questions = exam.getQuestions();
+
+			for (int i = 0; i < questions.size(); i++) {
+				System.out.println();
+
+				Exam.QuestionReference questionReference = questions.get(i);
+				Question question = questionReference.getQuestion();
+
+				System.out.printf("Question %d:%n", i + 1);
+				System.out.printf("\t%s%n", question.getText());
+
+				System.out.println("Answer:");
+				System.out.printf("\t%s%n", question.getAnswer());
+
+				System.out.print("Learning goals: ");
+				for (LearningGoal goal : question.getGoals()) System.out.printf("%s, ", goal.getDescription());
+				System.out.print("\b\b\n"); // Erase the final ", " and finish the line
+
+				System.out.printf("Length: %dm, Points: %d, Type: %s, Level: %s, Shortable: %s%n", question.getLength().toMinutes(), question.getPoints(), question.getType().name(), question.getLevel().name(), questionReference.isShortable() ? "Yes" : "No");
+			}
+
+			System.out.println();
+
+			System.out.println("1. Edit");
+			System.out.println("2. Back");
+		});
+
+		node.addConnection(input -> input.equals("2") || input.equalsIgnoreCase("back"), parent);
+		node.setDefaultTarget(node, _ -> System.out.println("Invalid option, please try again"));
 
 		return node;
 	}
